45b5f0247ac733104beaabb47c41caffe17bf6af
patterson-dtaylor <patterson.dtaylor@gmail.com>
2020-04-25 17:26:08 -0400

    adding and updating files

--- Tipsy/Controllers/CalculatorViewController.swift
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -15,16 +15,69 @@ class CalculatorViewController: UIViewController {
     @IBOutlet weak var tenPctButton: UIButton!
     @IBOutlet weak var twentyPctButton: UIButton!
     @IBOutlet weak var splitNumberLabel: UILabel!
+    
+    var tip = 0.0
+    var billTotal = 0.0
+    var numberOfPeople = 2
+    var finalResult = ""
 
     @IBAction func tipChanged(_ sender: UIButton) {
+        
+        // Dismisses keyboard when percentage buttons are pushed
+        billTextField.endEditing(true)
+        
+        // set all buttons as not selected
+        zeroPctButton.isSelected = false
+        tenPctButton.isSelected = false
+        twentyPctButton.isSelected = false
+        
+        // sets the selected button to show keeping the others off
+        sender.isSelected = true
+        
+        // sets the variable for the button pressed
+        let buttonTitle = sender.currentTitle!
+        
+        // sets the button without percentage mark
+        let buttonNumberWithoutPctSymbol = buttonTitle.dropLast()
+        
+        // converts button title into a double
+        let buttonTitleAsNumber = Double(buttonNumberWithoutPctSymbol)!
+        
+        // declare and initate the tip variable
+        tip = buttonTitleAsNumber / 100
+        
     }
     
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        splitNumberLabel.text = String(format: "%.0f", sender.value)
+        numberOfPeople = Int(sender.value)
     }
     
     @IBAction func calculatePressed(_ sender: UIButton) {
+        
+        let bill = billTextField.text!
+        
+        // tests to make sure there is data in the Fieldtext
+        if bill != "" {
+            billTotal = Double(bill)!
+            
+            let result = billTotal * (1 + tip) / Double(numberOfPeople)
+            
+            finalResult = String(format: "%.2f", result)
+            
+        }
+        
+        performSegue(withIdentifier: "goToResults", sender: self)
+        
     }
     
-    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "goToResults" {
+            let destinationVC = segue.destination as! ResultsViewController
+            destinationVC.totalBillPerPerson = finalResult
+            destinationVC.numberOfPeople = numberOfPeople
+            destinationVC.tipPct = tip
+        }
+    }
 }
 
--- Tipsy/Controllers/ResultsViewController.swift
+++ Tipsy/Controllers/ResultsViewController.swift
@@ -9,22 +9,27 @@
 import UIKit
 
 class ResultsViewController: UIViewController {
-
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var settingsLabel: UILabel!
+    
+    var totalBillPerPerson: String?
+    var numberOfPeople: Int?
+    var tipPct: Double?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        let number = String(numberOfPeople!)
+        let tip = String(format: "%.0f", tipPct! * 100)
+        
+        
+        totalLabel.text = totalBillPerPerson
+        settingsLabel.text = "Split between \(number) people, with \(tip)% tip."
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func reclculatePressed(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
     }
-    */
 
 }
